7fa92f2d4c2545ddb94e0c7ba52608717b511d56
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-09 02:53:00 +0300

    Create Item component

---
+++ frontend/src/components/item/item.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './item.module.css'
+import { NavLink, Route } from 'react-router-dom'
+
+const Item = ({ data }) => {
+    const { image, brand, model, color, price } = data
+
+    return (
+        <NavLink to='/item' className={styles.item}>
+            <div className={styles.item__container}>
+                <div className={styles.item__image}>
+                    <img src={image}></img>
+                </div>
+
+                <div className={styles.item__desc}>
+                    <div>
+                        <div className={styles.item__desc_brand}>{brand}</div>
+                        <div className={styles.item__desc_model}>{model}</div>
+                        <div className={styles.item__desc_color}>{color}</div>
+                    </div>
+                    <div className={styles.item__desc_price}>{price} р</div>
+
+                </div>
+            </div>
+        </NavLink>
+    )
+}
+
+
+export default Item
