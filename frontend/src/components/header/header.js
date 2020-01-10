2bb1cf16d724f0e4bfb8bd0f4c66b49deeaa9e68
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-11 00:13:30 +0300

    imported NavLink Routes

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -2,23 +2,30 @@ import React from 'react';
 import styles from './header.module.css'
 import find from '../../images/header-images/find.svg'
 import menu from '../../images/header-images/menu.svg'
+import { NavLink } from 'react-router-dom'
 
 const Header = props => {
     return (
         <header>
             <div className={styles.header__container}>
                 <div>
-                    <h2>SOUTHYYY</h2>
+                    <NavLink to='/home'>
+                        <h2>SOUTHYYY</h2>
+                    </NavLink>
                 </div>
                 <div className={styles.header__navbar}>
                     <ul>
-                        <li>Продать</li>
-                        <li>Профиль</li>
+                        <NavLink to='/sale'>
+                            <li>Продать</li>
+                        </NavLink>
+                        <NavLink to='/profile'>
+                            <li>Профиль</li>
+                        </NavLink>
                         <li>
-                            <img src={find} alt='find'></img> 
+                            <img src={find} alt='find'></img>
                         </li>
                         <li>
-                            <img src={menu} alt='menu'></img> 
+                            <img src={menu} alt='menu'></img>
                         </li>
                     </ul>
                 </div>
