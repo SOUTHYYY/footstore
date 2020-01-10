0645318b664007fd1ee3325c9400ec39b2eec281
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-11 00:11:47 +0300

    add cors and header settings
    add cors and header settings to send data to the frontend normally

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const app = express()
-
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-require('dotenv/config')
+const bodyParser = require('body-parser')
+const postsRoute = require('./routes/posts')
+const authRoute = require('./routes/auth')
+const cors = require('cors');
+
+dotenv.config()
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json())
+
+app.use('/auth', authRoute)
+app.use('/posts', postsRoute)
+
 
 mongoose.connect(
-    'mongodb+srv://southyyy:LehaS30072002@foot-store-cluster-rkttp.mongodb.net/test?retryWrites=true&w=majority',
-    {useNewUrlParser: true, useUnifiedTopology: true},
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log('connected to DB!')
 )
 
-app.listen(4000)
 
+// How do we start listening to the server? 
+app.listen(4000)
