6829eacf6a23f98ccb7d3710ef5ee09d186a1eb0
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-15 21:47:02 +0300

    Add card-reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from "redux";
 import auth from './auth-reducer'
 import posts from './posts-reducer'
+import card from './item-card-reducer'
 
 
 export default combineReducers({
     auth,
-    posts
+    posts,
+    card
 })
