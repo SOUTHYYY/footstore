1d96c7994f660a2b616a021a5431c6c5bd25af4b
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-19 16:55:14 +0300

    combine catalogReducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import auth from './auth-reducer'
 import posts from './posts-reducer'
 import card from './item-card-reducer'
+import catalog from './catalog-reducer'
 
 
 export default combineReducers({
+    catalog,
     auth,
     posts,
     card
